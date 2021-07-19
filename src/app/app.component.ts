c3392827ccb9f031a8f9e16cc66563c93b016ac3
Saida Castro <saida.castro.at@gmail.com>
2021-07-19 10:28:11 +0200

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //templateUrl: 'app.component.html',
+  //template: '<span>Saida</span>',
+  //template: `<h1>Hola de nuevo</h1>` ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorComponent } from './contador/contador/contador.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
